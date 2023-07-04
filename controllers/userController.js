77dbcbe03e87d01ba070598c0dd65273da527bb3
ZymanteL <zymantelelesiute@gmail.com>
2023-07-04 14:22:25 +0300

    mongoose-methods-task

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,37 @@
 import userModel from "../modules/userModel.js";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+
+export const loginUser = async (reg, res) => {
+  try {
+    const foundUser = await userModel.findOne({ userName: req.body.userName });
+    if (!foundUser) {
+      return res.status(404).send(`Username or password is wrong`);
+    }
+    
+    const isUserPasswordCorrect = bcrypt.compareSync(req.body.password.toString(), userExists.password)
+    if(!isUserPasswordCorrect) {
+      return res.status(404).send(`Username or password is wrong!`);
+    }
+
+    const token = jwt.sign({id: foundUser._id}, process.env.JWT_SECRET, {expiresIn: 60})
+
+    return res.cokie("session_token", token, {
+      httpOnly:true,
+    }).status(200).send(`Hello ${foundUser.name}`)
+    console.log('hey you are logged in!');
+    res.status(200).send(error)
+  } catch (error) {}
+};
 
 export const createUser = async (req, res) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(req.body.password.toString(), salt);
     const newUser = new userModel({
       ...req.body,
+      password: hashPassword,
+      isAdmin: false,
     });
     await newUser.save();
 
@@ -62,10 +90,54 @@ export const updateUser = async (req, res) => {
       },
       { new: true }
     );
-    
+
     res.status(201).json(user);
   } catch {
     console.log(error);
     res.status(400).send(error);
   }
 };
+
+export const countUsers = async (req, res) => {
+  
+  try {
+      const {userName} = req.body
+      const countedUsers = await userModel.countDocuments({userName})
+      res.status(201).send(countedUsers.toString())
+      
+  
+  } catch (error) {
+      console.log(error)
+      res.status(400).send(error)
+  }
+}
+
+export const sortUsers = async (req, res) => {
+  
+  try {
+      const sortedUsers = await userModel.find({}, {password:0}).sort({userName:'asc'})
+      
+     
+      res.status(201).send(sortedUsers)
+      
+  
+  } catch (error) {
+      console.log(error)
+      res.status(400).send(error)
+  }
+}
+
+export const limitUsers = async (req, res) => {
+    
+  try {
+      const sortedUsers = await userModel.find({}, {password:0}).sort({userName:'desc'}).limit(1)
+      
+     
+      res.status(201).send(sortedUsers)
+      
+  
+  } catch (error) {
+      console.log(error)
+      res.status(400).send(error)
+  }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { createUser, deleteUser, getAllUsers, getUser, updateUser } from "../controllers/userController.js";
+import { createUser, deleteUser, getAllUsers, getUser, loginUser, updateUser, countUsers, sortUsers, limitUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/create", createUser);
+router.post("/login", loginUser);
 router.get("/get-all", getAllUsers);
 router.get("/get", getUser);
 router.delete("/delete", deleteUser);
 router.put("/update", updateUser);
+router.get('/count', countUsers)
+router.get('/sort', sortUsers)
+router.get('/limit', limitUsers)
 
 export default router;
